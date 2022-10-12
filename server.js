05ecfc19ec352a8bb410f2913fdc134890843b5e
rlaqorrb <rlaqorrb20050514@gmail.com>
2022-10-12 15:39:28 +0900

    tl

--- server.js
+++ server.js
@@ -10,45 +10,59 @@ MongoClient.connect('mongodb+srv://rlaqorrb:rlatpdms0911@cluster0.hrzuyuj.mongod
 
   if (err) return console.log(err)
   db = client.db('todoapp');
-/* 
-  db.collection('post').insertOne({ 이름: 'John', _id : 100}, function (err, res) {
-    console.log('저장완료');
-  }); */
+  /* 
+    db.collection('post').insertOne({ 이름: 'John', _id : 100}, function (err, res) {
+      console.log('저장완료');
+    }); */
   app.post('/add', (req, res) => {
-    res.redirect('/write');
-    console.log(req.body.title);
-    console.log(req.body.date);
-    db.collection('post').insertOne({제목 : req.body.title, 날짜 : req.body.date})
-  })
+    db.collection('counter').findOne({ name: '게시물갯수' }, (err, result) => {
+      console.log(result.totalPost);
+      var 총게시물갯수 = result.totalPost;
+
+      db.collection('post').insertOne({ 게시물번호: 총게시물갯수 + 1, 제목: req.body.title, 날짜: req.body.date }, () => {
+        console.log('저장완료');
+
+        db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (err, result) {
+          if (err) return console.log(err);
+        }) // $set : 변경, $inc : 증가, 
+      });
+    });
+    res.redirect('/list');
+  });
 
 
   app.listen(8080, function () {
     console.log('listening on 8080')
   });
 
-})
+});
 
 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-})
+});
 
 app.get('/write', (req, res) => {
   res.sendFile(__dirname + '/write.html');
-})
+});
 
-app.get('/index.css', (req, res) =>{
+app.get('/index.css', (req, res) => {
   res.sendFile(__dirname + '/index.css');
-})
+});
 
 app.get('/write.css', (req, res) => {
   res.sendFile(__dirname + '/write.css');
-})
+});
 
 app.get('/list', (req, res) => {
-  db.collection('post').find().toArray((err, res) => {
-    console.log(res);
-    res.render('list.ejs', {posts : res});
+  db.collection('post').find().toArray((err, result) => {
+    console.log(result);
+    res.render('list.ejs', { posts: result });
   });
-})
+});
+
+app.delete('/delete', (req, res) => {
+  console.log(req.body);
+});
+
